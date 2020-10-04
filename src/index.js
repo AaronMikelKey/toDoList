957ed0de3f1794eb8e2925ce75b87b9c7582db42
buckshot307 <buckshot307@gmail.com>
2020-10-04 11:17:42 -0400

    updated html skeleton

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './styles/style.css';
 require('./styles/mystyles.scss');
+const dayjs = require('dayjs');
+
+//sets now to current date
+const now = dayjs();
+//sets format to e.g. Oct 03, 2020
+const dateF = 'MMM DD, YYYY';
+//Oct 03, 2020
+document.getElementById('date').innerHTML = "Due on :" + now.format(dateF);
