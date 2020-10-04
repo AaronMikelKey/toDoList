75432d068ad8f58ba22ee5320facb60d57a4e696
buckshot307 <buckshot307@gmail.com>
2020-10-04 17:16:34 -0400

    Got customParseFormat to work

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './styles/style.css';
 require('./styles/mystyles.scss');
 const dayjs = require('dayjs');
+const customParseFormat = require('./components/customParseFormat.js');
+dayjs.extend(customParseFormat);
 
 //sets now to current date
 const now = dayjs();
 //sets format to e.g. Oct 03, 2020
 const dateF = 'MMM DD, YYYY';
 //Oct 03, 2020
-document.getElementById('date').innerHTML = "Due on :" + now.format(dateF);
+document.getElementById('dueDate').innerHTML = dayjs('2020-03-19').format(dateF);
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [new MiniCssExtractPlugin(),],
     module: {
       rules: [
         {
