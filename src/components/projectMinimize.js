df27a3c6abab36829875b364e96e77bf7eb1b097
buckshot307 <buckshot307@gmail.com>
2020-10-05 13:40:33 -0400

    Fixed hiding of project list when clicking arrow next to project name

--- src/components/projectMinimize.js
+++ src/components/projectMinimize.js
@@ -1,18 +1,19 @@
 export default function projectMinimize() {
+    const displayArr = ['inline-block', 'block'];
     if (this.classList.contains('fa-angle-down')) {
     this.classList.remove('fa-angle-down');
     this.classList.add('fa-angle-right');
     let siblings = this.parentNode.childNodes;
     for (let i=2;i<siblings.length;i++){
-        console.log(siblings[i])
-        siblings[i].style.visibility = 'hidden';
+        siblings[i].style.display = 'none';
     }
     } else {
         let siblings = this.parentNode.childNodes;
-        for (let i=2;i<siblings.length;i++) {
+        for (let i=3;i<siblings.length;i++) {
         this.classList.remove('fa-angle-right');
         this.classList.add('fa-angle-down');
-        siblings[i].style.visibility = 'visible';
+        siblings[2].style.display = 'inline-block';
+        siblings[i].style.display = 'block';
         }
     }
 }
